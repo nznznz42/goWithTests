0b7cbf03344ebe619da224c62c46b53fd7810831
nznznz42 <coolcarrot420@gmail.com>
2023-11-07 19:22:12 +0530

    End Of chapter

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -3,24 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+	t.Run("to a person", func(t *testing.T) {
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+	t.Run("empty string", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("in Spanish", func(t *testing.T) {
-		got := Hello("Elodie", "Spanish")
+		got := Hello("Elodie", spanish)
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", french)
+		want := "Bonjour, Lauren"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {

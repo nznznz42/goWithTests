c4c8aa4929bad3ce6bb6db582c362e1886e27111
nznznz42 <coolcarrot420@gmail.com>
2023-11-07 19:25:50 +0530

    integer test

---
+++ integers/adder_test.go
@@ -0,0 +1,20 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected: '%d'\n recieved: '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+}

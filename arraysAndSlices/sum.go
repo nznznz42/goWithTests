f21c608ba68815099f3e7b677c739a45c46c1ebc
nznznz42 <coolcarrot420@gmail.com>
2023-11-07 20:20:16 +0530

    array and slices test

---
+++ arraysAndSlices/sum.go
@@ -0,0 +1,20 @@
+package main
+
+func Sum(numbers []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}

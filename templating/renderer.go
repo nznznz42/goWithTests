2efa5c1e611bd406889eb040144bdc25d7440a73
nznznz42 <coolcarrot420@gmail.com>
2023-12-13 00:18:03 +0530

    continuation

--- templating/renderer.go
+++ templating/renderer.go
@@ -9,17 +9,14 @@ import (
 )
 
 var (
-	//go:embed "templates/*"
 	postTemplates embed.FS
 )
 
-// PostRenderer renders data into HTML
 type PostRenderer struct {
 	templ    *template.Template
 	mdParser *parser.Parser
 }
 
-// NewPostRenderer creates a new PostRenderer
 func NewPostRenderer() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
@@ -32,12 +29,10 @@ func NewPostRenderer() (*PostRenderer, error) {
 	return &PostRenderer{templ: templ, mdParser: parser}, nil
 }
 
-// Render renders post into HTML
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
 	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r))
 }
 
-// RenderIndex creates an HTML index page given a collection of posts
 func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
 	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
 }
